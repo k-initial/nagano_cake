7ce8a16ec900bcfa07fb4d8c415cb2291add6d39
EC2 Default User <ec2-user@ip-172-31-91-134.ec2.internal>
2022-06-21 02:51:36 +0000

    　　訂正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,14 +8,7 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :addresses, dependent: :destroy
   
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true 
-  validates :post_code, presence: true
-  varidates :address, presence: true
-  validates :phone_number, presence: true
-  validates :email, uniqueness: true
+  
   
   def self.search_for(content,method)
     return none if content.blank?
