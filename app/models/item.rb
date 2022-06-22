bfd844a41ae0ed2ccd5a14f55e125aa50c29e829
EC2 Default User <yaya0517k@gmail.com>
2022-06-22 04:18:46 +0000

    admin/item機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,35 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id              :integer          not null, primary key
+#  introduction    :text
+#  is_sales_status :boolean          default(TRUE)
+#  name            :string
+#  non_taxed_price :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  genre_id        :integer
+#  item_image_id   :string
+#
 class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   belongs_to :genre
   has_many :orders, through: :order_details
-
+  
+  has_one_attached :image
+  
+  def get_image(width, height)
+   unless image.attached?
+     file_path = Rails.root.join('app/assets/images/default-image.jpeg')
+     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+   end
+     image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def with_tax_price
+     (non_taxed_price * 1.1).floor
+  end
+  
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id             :integer          not null, primary key
+#  address        :string
+#  name           :string
+#  order_status   :integer          default(0), not null
+#  payment_method :integer          default(0), not null
+#  post_code      :string
+#  postage        :integer
+#  total_payment  :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  customer_id    :integer
+#
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
