018d6f634df18ff280ebc23339b9bbc11b274bc4
EC2 Default User <yaya0517k@gmail.com>
2022-06-26 08:02:14 +0000

    ログイン画面修正

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -19,8 +19,9 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
          
          has_one_attached :image
 end
+# :registerable,
